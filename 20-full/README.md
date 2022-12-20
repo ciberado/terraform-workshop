4a66ca302140b20de3943cd9dbba7d30311a298b
Javi Moreno <email@javier-moreno.com>
2022-12-20 12:09:40 +0100

    Basic structure for new example

---
+++ 20-full/README.md
@@ -0,0 +1,13 @@
+# Simple module demo (for VPC creation)
+
+This demo will take advantage of the [vpc module](https://github.com/terraform-aws-modules/terraform-aws-vpc) to create a fully-featured multi-az public/private VPC.
+
+```
+terraform init
+terraform plan
+terraform apply
+```
+
+```
+terraform destroy
+```
---
+++ 20-full/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "pokemon_vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+
+  enable_s3_endpoint = true
+
+  enable_flow_log                      = true
+  create_flow_log_cloudwatch_log_group = true
+  create_flow_log_cloudwatch_iam_role  = true
+  flow_log_max_aggregation_interval    = 60
+
+  tags = {
+    Name    = "pokemon-vpc"
+    Owner   = "ciberado"
+    Project = "terraform-pokemon-demo"
+  }
+}
